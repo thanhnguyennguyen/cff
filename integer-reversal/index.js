9318cda815f18b6844858b721bd39fc00053a1f2
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-16 16:06:25 +0700

    integer-reversal

--- integer-reversal/index.js
+++ integer-reversal/index.js
@@ -2,16 +2,7 @@
 
 const reverseInt = (num) => {
     // put your code here to address problems
-    var str = num.toString(),
-        sign = 1;
-    if (str[0] === '-') {
-        str = str.slice(1);
-        sign = -1;
-    }
-    debugger;
-    return sign * parseInt(str.split('').reverse().join(''));
+    return Math.sign(num) * parseInt(num.toString().split('').reverse().join(''));
 }
-
-reverseInt(123);
 
 module.exports = reverseInt;
