3e319aa207c0d5a227a7cf6099738723dbe1bf10
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-16 15:42:53 +0700

    integer-reversal

--- finish.sh
+++ finish.sh
@@ -3,8 +3,7 @@ if [[ $# -eq 0 ]] ; then
     echo "Please specify name of the problem that you have finished "
     exit 1
 fi
-mkdir $1;
 git checkout -b $1
 git add $1
-git commit "$1"
+git commit -m "$1"
 git push origin $1
