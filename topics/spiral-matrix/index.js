99047948519081e1ff1ba2a3c4d90a98fbbb2b73
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-19 18:35:06 +0700

    spiral-matrix: refactor

--- topics/spiral-matrix/index.js
+++ topics/spiral-matrix/index.js
@@ -1,3 +1,23 @@
+const goLeft = (num, val = 1, col = 0, row = 0, result = [[]]) => {
+    return spiral(num, val + 1, col - 1, row, true, result);
+}
+
+const goRight = (num, val = 1, col = 0, row = 0, result = [[]]) => {
+    return spiral(num, val + 1, col + 1, row, true, result);
+}
+
+const goDown = (num, val = 1, col = 0, row = 0, result = [[]]) => {
+    return spiral(num, val + 1, col, row + 1, false, result);
+     ;
+}
+
+const goUp = (num, val = 1, col = 0, row = 0, result = [[]]) => {
+    return spiral(num, val + 1, col, row - 1, false, result);
+}
+
+const visitedPosition = (col, row, result) => {
+    return (result[row] !== undefined && result[row][(col)] !== undefined);
+}
 
 const spiral = (num, val = 1, col = 0, row = 0, horizontal = true, result = [[]]) => {
     // put your code here to address problems
@@ -5,41 +25,39 @@ const spiral = (num, val = 1, col = 0, row = 0, horizontal = true, result = [[]]
 
     if (!result[row]) result[row] = [];
     result[row][col] = val;
-    debugger;
     if (val === Math.pow(num, 2)) {
         return result;
     }
     if (horizontal) {
-        if ((col + 1) < num &&  (result[row] === undefined || result[row][(col + 1)] === undefined)) {
-            return spiral(num, val + 1, col + 1, row, true, result);
+        if ((col + 1) < num &&  !visitedPosition(col + 1, row, result)) {
+            return goRight(num, val, col, row, result);
         }
-        if ((col - 1) >= 0 &&  (result[row] === undefined || result[row][(col - 1)] === undefined)) {
-            return spiral(num, val + 1, col - 1, row, true, result);
+        if ((col - 1) >= 0 &&  !visitedPosition(col - 1, row, result)) {
+            return goLeft(num, val, col, row, result);
         }
-        if ((row + 1) < num &&  (result[row + 1] === undefined|| result[row + 1][col] === undefined)) {
-            return spiral(num, val + 1, col, row + 1, false, result);
+        if ((row + 1) < num &&  !visitedPosition(col, row + 1, result)) {
+            return goDown(num, val, col, row, result);
         }
-        if ((row - 1) >= 0 &&  (result[row - 1] === undefined|| result[row - 1][col] === undefined)) {
-            return spiral(num, val + 1, col, row - 1, false, result);
+        if ((row - 1) >= 0 &&  !visitedPosition(col, row - 1, result)) {
+            return goUp(num, val, col, row, result);
         }
     } else {
-        if ((row + 1) < num &&  (result[row + 1] === undefined|| result[row + 1][col] === undefined)) {
-            return spiral(num, val + 1, col, row + 1, false, result);
+        if ((row + 1) < num &&  !visitedPosition(col, row + 1, result)) {
+            return goDown(num, val, col, row, result);
         }
-        if ((row - 1) >= 0 &&  (result[row - 1] === undefined|| result[row - 1][col] === undefined)) {
-            return spiral(num, val + 1, col, row - 1, false, result);
+        if ((row - 1) >= 0 &&  !visitedPosition(col, row - 1, result)) {
+            return goUp(num, val, col, row, result);
         }
-        if ((col + 1) < num &&  (result[row] === undefined || result[row][(col + 1)] === undefined)) {
-            return spiral(num, val + 1, col + 1, row, true, result);
+        if ((col + 1) < num &&  !visitedPosition(col + 1, row, result)) {
+            return goRight(num, val, col, row, result);
         }
-        if ((col - 1) >= 0 &&  (result[row] === undefined || result[row][(col - 1)] === undefined)) {
-            return spiral(num, val + 1, col - 1, row, true, result);
+        if ((col - 1) >= 0 &&  !visitedPosition(col - 1, row, result)) {
+            return goLeft(num, val, col, row, result);
         }
 
     }
     
 }
 
-spiral(3);
 
 module.exports = spiral;
