5d0e9f7ca73176ca1a7e8cebfeeb4bee16281f57
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-20 17:28:25 +0700

    update init.sh script

--- init.sh
+++ init.sh
@@ -3,5 +3,8 @@ if [[ $# -eq 0 ]] ; then
     echo "Please specify a name for this problem"
     exit 1
 fi
+echo "Checkout laster master...";
+git checkout master;
+git pull;
 mkdir topics/$1;
 cp template/* topics/$1
