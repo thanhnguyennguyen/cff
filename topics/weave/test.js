f27efd31e83ed5fe57f8a87f469812c65d2d3809
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-20 20:38:20 +0700

    Weave: More unit tests

--- topics/weave/test.js
+++ topics/weave/test.js
@@ -12,7 +12,7 @@ test('template weave: q1.length === q2.length', () => {
     expect(weave(q1, q2).peek()).toEqual(["there!", 2, "Hi", 1]);
 });
 
-test('template weave: q1.length !== q2.length', () => {
+test('template weave: q1.length > q2.length', () => {
     let q1 = new Queue(),
         q2 = new Queue();
         q1.enqueue(1);
@@ -23,4 +23,18 @@ test('template weave: q1.length !== q2.length', () => {
         q2.enqueue('there!');
 
     expect(weave(q1, q2).peek()).toEqual([4, 3, "there!", 2, "Hi", 1]);
+});
+
+
+test('template weave: q1.length < q2.length', () => {
+    let q1 = new Queue(),
+        q2 = new Queue();
+        q1.enqueue(1);
+        q1.enqueue(2);
+        q2.enqueue('Hi');
+        q2.enqueue('there!');
+        q2.enqueue('I\'m');
+        q2.enqueue('Nguyen');
+
+    expect(weave(q1, q2).peek()).toEqual(['Nguyen', 'I\'m', "there!", 2, "Hi", 1]);
 });
