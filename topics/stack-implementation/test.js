f259f9526f2a1af560f017b7195e6a445cc56e70
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-20 21:43:43 +0700

    Stack: test top function

--- topics/stack-implementation/test.js
+++ topics/stack-implementation/test.js
@@ -3,8 +3,10 @@ const Stack = require('./index.js');
 test('test stack', () => {
     let stack = new Stack();
     stack.push(1);
+    expect(stack.top()).toEqual(1);
     stack.push(2);
     stack.push(3);
+    expect(stack.top()).toEqual(3);
     expect(stack.pop()).toEqual(3);
     expect(stack.pop()).toEqual(2);
     expect(stack.pop()).toEqual(1);
