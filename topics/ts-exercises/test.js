8bc40cfd6ac6a7274a9af3ddfd1d9800237ed902
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-09-06 17:22:28 +0700

    Find actual activation date: adding unit tests

--- topics/ts-exercises/test.js
+++
@@ -1,9 +0,0 @@
-const  {findActualActivationDate, processData} = require('./index.js');
-// put your tests here
-test('test findActualActivationDate', () => {
-    expect(findActualActivationDate()).toEqual();
-});
-
-test('test processData', () => {
-    expect(processData()).toEqual();
-});
