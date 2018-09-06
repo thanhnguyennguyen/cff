78ef422c9667274ddd2b1fec1f49bfbb2e0a8d5a
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-09-06 16:37:18 +0700

    Find actual activation date: read CSV input file

--- topics/ts-exercises/index.js
+++ topics/ts-exercises/index.js
@@ -3,4 +3,8 @@ const findActualActivationDate = () => {
     // put your code here to address problems
 }
 
-module.exports = findActualActivationDate;
+const processData = () => {
+    // put your code here to address problems
+}
+
+module.exports = {findActualActivationDate, processData};
--- topics/ts-exercises/script.js
+++ topics/ts-exercises/script.js
@@ -1,2 +1,18 @@
-const findActualActivationDate = require('./index.js');
-// put your execution script here
+const  {findActualActivationDate, processData} = require('./index.js');
+const csv = require('csvtojson');
+const csvFilePath = './data/input.csv';
+const readFile = async() => {
+    console.log('Step 1: Importing is starting....');
+    jsonArray = await csv().fromFile(csvFilePath);
+    console.log('Step 1: Importing is done');
+    return jsonArray; 
+}
+const start = async() => {
+    rawData = await readFile();
+    // console.log(rawData);
+}
+
+let rawData = [];
+let processedData = [];
+
+start();
