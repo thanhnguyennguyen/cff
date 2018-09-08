a2cf7b7839f5a79d30f141f207e92979f9a8e0c1
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-09-08 17:41:29 +0700

    Find actual activation date: update unit tests

--- topics/find-actual-activation-date/tests/findActualActivationDate.test.js
+++ topics/find-actual-activation-date/tests/findActualActivationDate.test.js
@@ -1,4 +1,4 @@
-const  {findActualActivationDate, } = require('../index.js');
+const  {findActualActivationDate, processData, validateData} = require('../index.js');
 // put your tests here
 describe('test findActualActivationDate', () => {
     test('empty data', () => {
@@ -6,139 +6,87 @@ describe('test findActualActivationDate', () => {
     });
 
     test('no owner transfer transaction, no renewal transaction', () => {
-        let input = {
-            '0987000001': [
-                {
-                    activatation: '2016-01-01',
-                    deactivatation: ''
-                }
-            ],
-            '0987000002': [
-                {
-                    activatation: '2016-02-01',
-                    deactivatation: ''
-                }
-            ]
-        };
-        let expected = [
+        let input = [
             {
-                '0987000001': '2016-01-01'
-            },
-            {
-                '0987000002': '2016-02-01'
+                phone: '0987000001',
+                activatation: '2016-01-01',
+                deactivatation: ''
             }
         ];
+        let expected = {
+                phone: '0987000001',
+                actualActivationDate: '2016-01-01'
+            };
 
         expect(JSON.stringify(findActualActivationDate(input))).toEqual(JSON.stringify(expected));
     });
 
-    test('no owner-transfer transaction, has renewal transactiona', () => {
-        let input = {
-            '0987000001': [
-                {
-                    activatation: '2016-01-01',
-                    deactivatation: '2016-03-01'
-                },
-                {
-                    activatation: '2016-03-01',
-                    deactivatation: '2016-05-01'
-                }
-            ],
-            '0987000002':  [
-                {
-                    activatation: '2016-02-01',
-                    deactivatation: '2016-03-01'
-                }
-            ]
-        };
-        let expected = [
+    test('no owner-transfer transaction, has renewal transactions', () => {
+        let input = [
             {
-                '0987000001': '2016-01-01'
+                phone: '0987000001',
+                activatation: '2016-01-01',
+                deactivatation: '2016-03-01'
             },
             {
-                '0987000002': '2016-02-01'
+                phone: '0987000001',
+                activatation: '2016-03-01',
+                deactivatation: '2016-05-01'
             }
         ];
+        let expected = {
+                phone: '0987000001',
+                actualActivationDate: '2016-01-01'
+            };
 
         expect(JSON.stringify(findActualActivationDate(input))).toEqual(JSON.stringify(expected));
     });
 
     
     test('has owner-transfer transactions, NO renewal transaction', () => {
-        let input = {
-            '0987000001': [
-                {
-                    activatation: '2016-01-01',
-                    deactivatation: '2016-03-01'
-                },
-                {
-                    activatation: '2016-04-01',
-                    deactivatation: '2016-05-01'
-                }
-            ],
-            '0987000002': [
-                {
-                    activatation: '2016-02-01',
-                    deactivatation: '2016-03-01'
-                },
-                {
-                    activatation: '2016-05-01',
-                    deactivatation: ''
-                }
-            ]
-        };
-        let expected = [
+        let input = [
             {
-                '0987000001': '2016-04-01'
+                phone: '0987000001',
+                activatation: '2016-01-01',
+                deactivatation: '2016-03-01'
             },
             {
-                '0987000002': '2016-05-01'
+                phone: '0987000001',
+                activatation: '2016-04-01',
+                deactivatation: '2016-05-01'
             }
         ];
+        let expected = {
+            phone: '0987000001',
+            actualActivationDate: '2016-04-01'
+        };
 
         expect(JSON.stringify(findActualActivationDate(input))).toEqual(JSON.stringify(expected));
     });
 
       
     test('has both owner-transfer transactions and renewal transactions', () => {
-        let input = {
-            '0987000001': [
-                {
-                    activatation: '2016-01-01',
-                    deactivatation: '2016-03-01'
-                },
-                {
-                    activatation: '2016-03-01',
-                    deactivatation: '2016-05-01'
-                },
-                {
-                    activatation: '2016-08-01',
-                    deactivatation: '2016-09-01'
-                }
-            ],
-            '0987000002': [
-                {
-                    activatation: '2016-02-01',
-                    deactivatation: '2016-03-01'
-                },
-                {
-                    activatation: '2016-03-01',
-                    deactivatation: '2016-05-01'
-                },
-                {
-                    activatation: '2016-08-01',
-                    deactivatation: ''
-                }
-            ]
-        };
-        let expected = [
+        let input = [
             {
-                '0987000001': '2016-08-01'
+                phone: '0987000001',
+                activatation: '2016-01-01',
+                deactivatation: '2016-03-01'
             },
             {
-                '0987000002': '2016-08-01'
+                phone: '0987000001',
+                activatation: '2016-03-01',
+                deactivatation: '2016-05-01'
+            },
+            {
+                phone: '0987000001',
+                activatation: '2016-08-01',
+                deactivatation: '2016-09-01'
             }
         ];
+        let expected = {
+            phone: '0987000001',
+            actualActivationDate: '2016-08-01'
+        };
 
         expect(JSON.stringify(findActualActivationDate(input))).toEqual(JSON.stringify(expected));
     });
---
+++ topics/find-actual-activation-date/tests/validation.test.js
@@ -0,0 +1,23 @@
+const  {findActualActivationDate, processData, validateData} = require('../index.js');
+// put your tests here
+
+describe('test validateData', () => {
+    test('test empty data', () => {
+        expect(validateData([])).toEqual(false);
+    });
+    test('test invalid number', () => {
+        expect(validateData(['016abc'])).toEqual(false);
+    });
+    test('test invalid date', () => {
+        expect(validateData(['0980000001', '2018-09-abc'])).toEqual(false);
+    });
+    test('test valid data, empty deactivation date', () => {
+        expect(validateData(['0980000001', '2018-09-01'])).toEqual(true);
+    });
+    test('test invalid data: activation > deactivation', () => {
+        expect(validateData(['0980000001', '2018-09-01', '2018-08-01'])).toEqual(false);
+    });
+    test('test valid data', () => {
+        expect(validateData(['0980000001', '2018-09-01', '2018-10-01'])).toEqual(true);
+    });
+});
