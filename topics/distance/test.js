c249d4512a6c28617a3dbdedde11eb2a2c477978
Nguyen Nguyen <nguyenbk92@gmail.com>
2018-10-18 19:29:59 +0700

    max distance: update tests

--- topics/distance/test.js
+++ topics/distance/test.js
@@ -1,5 +1,8 @@
-const distance = require('./index.js');
+const maxDistance = require('./index.js');
 // put your tests here
 test('test distance', () => {
-    expect(distance()).toEqual();
+    expect(maxDistance([1, 5, 2, 8, 10, 3])).toEqual(9);
+    expect(maxDistance([1])).toEqual(-1);
+    expect(maxDistance([1, 5])).toEqual(4);
+    expect(maxDistance([5, 2])).toEqual(-1);
 });
