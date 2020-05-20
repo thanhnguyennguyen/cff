d4497d285d0f50fc8ed1a22488f67c94b8270e66
NguyenNguyen <nguyenbk92@gmail.com>
2020-05-20 14:35:04 +0700

    Stack: postfix to prefix

---
+++ topics/stack/postfix-to-prefix/index.js
@@ -0,0 +1,25 @@
+const Stack = require('../index.js');
+const operators = {
+    "+": true,
+    "-": true,
+    "*": true,
+    "/": true,
+}
+const PostfixToPrefix = (formula) => {
+    let result = ""
+    let stack = new Stack()
+    for (let i = 0; i < formula.length; i++) {
+        let c = formula[i]
+        if (!operators[c]) {
+            stack.push(c)
+            continue
+        }
+        let subFormula = stack.pop()
+        subFormula = stack.pop() + subFormula
+        subFormula = c + subFormula
+        stack.push(subFormula)
+
+    }
+    return stack.pop()
+}
+module.exports = PostfixToPrefix
